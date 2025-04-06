7623f67f3ecede15ed7cd7f2378991c98ee4651f
Disha <d4dishamani.das@gmail.com>
2025-04-06 11:15:00 +0530

    script

--- server.py
+++ server.py
@@ -4,13 +4,14 @@ eventlet.monkey_patch()
 
 from flask import Flask, request
 from flask_socketio import SocketIO
+from flask import render_template
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 @app.route('/')
-def index():
-    return "IoT Flask Server Running with WebSocket!"
+def home():
+    return render_template('index.html')
 
 @app.route('/data', methods=['POST'])
 def receive_data():
